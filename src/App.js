4d6690dc9fc6ae2d1949f21615881838640221da
Teddy Mwai <teddymwai11@gmail.com>
2022-06-08 13:31:30 +0300

    cleaned up files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { popularGamesUrl } from "./api";
 
 function App() {
+  const getGames = async () => {
+    try {
+      const response = await fetch(popularGamesUrl());
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getGames();
+
   return (
     <div className="App">
       <h1>Hello Ignite</h1>
---
+++ src/api.js
@@ -0,0 +1,36 @@
+
+// Getting date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const date = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+console.log(date);
+console.log(lastYear);
+
+getCurrentMonth();
+getCurrentDay();
+// Base URl
+const base_url = `https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}`;
+
+// Popular games
+const popular_games = `&dates=${lastYear},${date}&ordering=-rating&page_size=10`;
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
